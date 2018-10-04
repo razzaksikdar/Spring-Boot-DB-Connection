9fe4cb90871e745303914c5ac929eb5c2cdf9714
razzaksikdar <arsikdar@gmail.com>
2018-10-04 02:38:40 +0100

    first

--- src/main/java/com/qa/DBCon/controller/DBConController.java
+++ src/main/java/com/qa/DBCon/controller/DBConController.java
@@ -4,54 +4,63 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import org.springframework.web.bind.annotation.*; 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.DBCon.exception.ResourceNotFoundException;
 import com.qa.DBCon.model.mySpringBootDataModel;
+import com.qa.DBCon.repository.mySpringBootRepo;
 
-import SpringDataAccessPackage.personRepository;
+
 
 @RestController
 @RequestMapping("/api")
 public class DBConController {
 	
+//	@Autowired
+//	mySpringBootRepo repos;
+//	@Autowired
+//	mySpringBootRepo Repo;
 	@Autowired
-	personRepository myRepo;
+	mySpringBootRepo Repo;
 
 	// method to create a person 
 	@PostMapping("/person")
 	private mySpringBootDataModel createPerson(@Valid @RequestBody mySpringBootDataModel mSDM) {
-		return myRepo.save(mSDM);
+		return Repo.save(mSDM);
 	}
 	
 	@GetMapping("/person/{id}")
 	public mySpringBootDataModel getPersonbyID(@PathVariable(value="id")Long personID){
 		
-		return myRepo.findById(personID).orElseThrow(()->new ResourceNotFoundException("mySpringBootDataModel", "id", personID));
+		return Repo.findById(personID).orElseThrow(()->new ResourceNotFoundException("mySpringBootDataModel", "id", personID));
 	}
 	
 	@GetMapping("/person")
 	public List<mySpringBootDataModel> getAllPeople()
 	{
-		return myRepo.findAll();
+		return Repo.findAll();
 	}
 	
 	@PutMapping("/person/{id}")
 	public mySpringBootDataModel updatePerson(@PathVariable(value="id")Long personID,
 			@Valid@RequestBody mySpringBootDataModel personDetails)
 	{
-		mySpringBootDataModel mSDM = myRepo.findById(personID).orElseThrow(()->new ResourceNotFoundException("mySpringBootDataModel", "id", personID));
+		mySpringBootDataModel mSDM = Repo.findById(personID).orElseThrow(()->new ResourceNotFoundException("mySpringBootDataModel", "id", personID));
 		
 		mSDM.setName(personDetails.getName());
 		mSDM.setAddress(personDetails.getAddress());
 		mSDM.setAge(personDetails.getAge());
 		
-		mySpringBootDataModel updateData = myRepo.save(mSDM);
+		mySpringBootDataModel updateData = Repo.save(mSDM);
 		return updateData;
 	}
 	
@@ -59,9 +68,9 @@ public class DBConController {
 	@DeleteMapping("/person/{id}")
 	public ResponseEntity<?> deletePerson(@PathVariable(value="id")Long personID)
 	{
-		mySpringBootDataModel mSDM = myRepo.findById(personID).orElseThrow(()->new ResourceNotFoundException("mySpringBootDataModel", "id", personID));
+		mySpringBootDataModel mSDM = Repo.findById(personID).orElseThrow(()->new ResourceNotFoundException("mySpringBootDataModel", "id", personID));
 		
-		myRepo.delete(mSDM);
+		Repo.delete(mSDM);
 		return ResponseEntity.ok().build();
 	}
 	
